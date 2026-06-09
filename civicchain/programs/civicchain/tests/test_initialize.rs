2ff46443637661e5fdad68509e210d6672dc3d3f
Anirudhh <anirudhhsrinivasan309@gmail.com>
2026-06-09 21:54:27 +0530

    feat: complaint submit + bid + escrow logic

---
+++ civicchain/programs/civicchain/tests/test_initialize.rs
@@ -0,0 +1,12 @@
+use anchor_lang::prelude::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_placeholder() {
+        // tests will go here after deployment
+        assert!(true);
+    }
+}
